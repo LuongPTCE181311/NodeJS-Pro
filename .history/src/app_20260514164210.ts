2a67a1eed3aac53ac83380d4052746e2c5f4f0b9
LuongPTCE181311 <pluongp123@gmail.com>
2026-05-15 10:29:42 +0700

    video 29

---
+++ .history/src/app_20260514164210.ts
@@ -0,0 +1,16 @@
+// const express = require("express");
+import express from "express";
+const PORT = 8080;
+const app = express();
+
+app.get("/", (req, res) => {
+  res.send("Hello World");
+});
+
+app.get("/hoidanit", (req, res) => {
+  res.send("Hello ERic");
+});
+
+app.listen(8080, () => {
+  console.log(`My app is running on port : ${PORT}`);
+});
---
+++ .history/src/app_20260515102857.ts
@@ -0,0 +1,16 @@
+// const express = require("express");
+import express from "express";
+const PORT = 8080;
+const app = express();
+
+app.get("/", (req, res) => {
+  res.send("Hello World Update");
+});
+
+app.get("/hoidanit", (req, res) => {
+  res.send("Hello ERic Update");
+});
+
+app.listen(8080, () => {
+  console.log(`My app is running on port : ${PORT}`);
+});
